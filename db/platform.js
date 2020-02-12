e982bf127c002057a801419fce01d42490eec180
Alexey Tikhonenko <tihonenko999@gmail.com>
2020-02-12 17:39:31 +0500

    node server

--- db/platform.js
+++ db/platform.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   id: 1,
   active_color: '#ff7623',
   title: 'Mkset',
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('index.js')
+const db = require('./index.js')
+const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 
