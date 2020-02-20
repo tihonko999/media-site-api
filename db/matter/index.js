ac88021379645f7fcaebdf15a034a54c39f3bcab
Alexey Tikhonenko <tihonenko999@gmail.com>
2020-02-20 18:32:02 +0500

    matter/page content blocks

--- db/matter/index.js
+++ db/matter/index.js
@@ -1,14 +1,14 @@
 const text = require('./media/text')
 const image = require('./media/image')
 const gallery = require('./media/gallery')
+const video = require('./media/video')
 const file = require('./media/file')
-// const widget = require('./media/widget')
-// const video = require('./media/video')
+const widget = require('./media/widget')
 
 module.exports = {
   id: 1,
   title: 'Действительно легкий старт: регистрировать бизнес со Сбербанком стало намного проще',
   lead: 'ПАО «Сбербанк» и АО «Деловая среда» расширяют географию и формы оказания услуг по бесплатной регистрации бизнеса.',
   img: 'https://avatars.mds.yandex.net/get-zen_doc/153246/pub_5c9c3cc67825b400af148e37_5c9c7c35687bdd00b34dc423/scale_600',
-  content_blocks: [text, image, text, gallery, text, text, file, text, text],
+  content_blocks: [text, image, text, gallery, text, video, text, file, text, widget, text],
 }
--- db/pages.js
+++ db/pages.js
@@ -1,12 +1,10 @@
 const text = require('./matter/media/text')
 const image = require('./matter/media/image')
 const gallery = require('./matter/media/gallery')
-const video = require('./matter/media/video')
 const file = require('./matter/media/file')
-const widget = require('./matter/media/widget')
 
 module.exports = [{
   id: 'contacts',
   title: 'Контакты',
-  content_blocks: [text, image, text, gallery, text, video, text, file, text, widget, text],
+  content_blocks: [text, image, text, gallery, text, text, file, text, text],
 }]
