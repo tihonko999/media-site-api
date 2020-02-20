5bdad5b8d37213d78c5feede6337c43fd07af02b
Alexey Tikhonenko <tihonenko999@gmail.com>
2020-02-20 19:01:52 +0500

    off widget in matter

--- db/matter/media/text.js
+++ db/matter/media/text.js
@@ -2,5 +2,15 @@ module.exports = {
   type: 'text',
   content: `<p>faker.js is a popular project used by many organizations and individuals in production settings. Major and Minor version releases are generally on a monthly schedule. Bugs fixes are addressed by severity and fixed as soon as possible.</p>
 <p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
+<p>Permission is hereby <i>granted</i>, free of charge <a href="https://ya.ru">Ссылка</a> Permission is hereby <b>granted</b>, free of charge</p>
 `,
 }
