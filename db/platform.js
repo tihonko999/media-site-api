1a9a597fdce70401285d84318da6195377db847c
Alexey <tihonenko999@gmail.com>
2020-02-25 18:29:55 +0500

    platform counters

--- db/platform.js
+++ db/platform.js
@@ -3,6 +3,8 @@ module.exports = {
   active_color: '#ff7623',
   title: 'Mkset',
   favicon: 'https://mkset.ru/favicon.ico',
+  yandex_metrika_id: '10449379',
+  google_analytics_id: 'UA-45258083-1',
   banners: [
     {
       slug: 'rightcol',
