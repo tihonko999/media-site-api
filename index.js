604787981ae15e070132cf09583516259d497bb1
Alexey <tihonenko999@gmail.com>
2020-02-26 14:06:09 +0500

    server.js -> index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const jsonServer = require('json-server')
-const index = jsonServer.create()
+const server = jsonServer.create()
 const db = require('./db')
 const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 4000
 
-index.use(middlewares)
-index.use(router)
-
-index.listen(port)
+server.use(middlewares)
+server.use(router)
+server.listen(port)
